2aa167127bee1eb489b5a5f8715cb434110b3f48
MohitAgnihotri <mohit.agnihotri@signify.com>
2021-05-11 22:18:03 +0200

    Fixed partial read from the TCP stream.

--- main.c
+++ main.c
@@ -69,7 +69,7 @@ int main(int argc, char *argv[])
     sem_init(&sem_io,0,1);
 
     /*Setup the file pointer for the log file*/
-    fp = fopen("./dns_svr.log","wb");
+    fp = fopen("./dns_svr.log","w");
 
     /* Initialise pthread attribute to create detached threads. */
     if (pthread_attr_init(&pthread_client_attr) != 0) {
@@ -204,126 +204,142 @@ void *pthread_routine(void *arg)
 
     /* Setup UDP client socket for upstream server */
     int upstream_server_sockfd = SetupUpstreamServerSocket(next_hierarchy_dns_server_name);
-    int bytes_read = read(client_socket, buffer, SOCKET_BUFF_SIZE);
 
-    if (bytes_read > 0)
+    // Read 2 bytes from the TCP socket
+    unsigned short dns_request_length = 0;
+    int bytes_read = read(client_socket, buffer, sizeof(dns_request_length));
+    if (bytes_read > 0 && bytes_read == 2)
     {
-        message_t dns_request_msg;
-        memset(&dns_request_msg, 0x00, sizeof(dns_request_msg));
-
-        int dns_request_length = buffer[0] << 16 | buffer[1];
-
-        decode_dns_msg( &dns_request_msg, &buffer[2], dns_request_length);
-        int incoming_request_id = dns_request_msg.id;
-        print_message(&dns_request_msg);
-
-        // enter critical section
-        // 1. Open file
-        // 2. Write to the file.
-        // 3. Close the file
-        // Exit Critical section
-        sem_wait(&sem_io);
-        updatefile_requested(fp,dns_request_msg.questions->qName);
-        if (dns_request_msg.questions->qType != AAAA_Resource_RecordType)
+        dns_request_length = buffer[0] << 16 | buffer[1];
+        int len = 0;
+        memset(buffer, 0x00, SOCKET_BUFF_SIZE);
+        while (len != dns_request_length)
         {
-            updatefile_unimplemented_request(fp);
-        }
-        sem_post(&sem_io);
-
-        if (dns_request_msg.questions->qType == AAAA_Resource_RecordType)
-        {
-            // Check if the request can be served from local cache
-
-            // send the DNS request to next level server
-            send_dns_request(upstream_server_sockfd, next_hierarchy_dns_server_name, &buffer[2], dns_request_length);
-
-            // Wait for the response
-            memset(buffer, 0x00, SOCKET_BUFF_SIZE);
-            socklen_t len;
-            struct sockaddr_in     servaddr;
-            int bytes_received = recvfrom(upstream_server_sockfd, buffer, SOCKET_BUFF_SIZE,MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-            if (bytes_received <= 0)
+            int readlen = read(client_socket, &buffer[len], dns_request_length);
+            if (readlen > 0)
             {
-                perror("error in recvfrom");
+                len += readlen;
             }
-
-            memset(&dns_request_msg, 0x00, sizeof(dns_request_msg));
-
-            decode_dns_msg( &dns_request_msg, buffer, bytes_received);
-            sem_wait(&sem_io);
-            updatefile_ipaddress(fp, &dns_request_msg);
-            sem_post(&sem_io);
-
-            // Send the response to the server
-            uint8_t *response_msg = malloc(sizeof(char) * bytes_received + 2);
-            uint8_t *resp_ptr = response_msg;
-            memset(response_msg, 0x00, sizeof(char) * (bytes_received + 2));
-
-            // add length
-            put16bits(&response_msg,bytes_received);
-
-            // add oroginal request
-            put16bits(&response_msg,incoming_request_id);
-
-            // copy the response received from the server
-            memcpy(response_msg, &buffer[2], bytes_received-2);
-
-            int bytes_written = write(client_socket,resp_ptr,bytes_received+2);
-            if (bytes_written <= 0)
+            else
             {
-                perror("error in write");
-            }
-
-            // Update the cache
-        }
-        else
-        {
-            uint8_t *response_msg = malloc(1024);
-            uint8_t *resp_ptr = response_msg + 2;
-
-            // send Error Record for the Non-AAAA request
-            message_t error_message;
-            memset(&error_message, 0x00, sizeof(error_message));
-
-            // set id
-            error_message.id = incoming_request_id;
-
-            // Set the type of message
-            error_message.byte.u.qr = 1;
-            error_message.byte.u.Opcode = 0;
-            error_message.byte.u.aa = 0;
-            error_message.byte.u.tc = 0;
-            error_message.byte.u.rd = 0;
-            error_message.byte.u.ra = 0;
-            error_message.byte.u.z = 0;
-            error_message.byte.u.rcode = NotImplemented_ResponseType;
-
-            // leave most values intact for response
-            error_message.qdCount = 0; /* Question Count */
-            error_message.anCount = 0; /* Answer Record Count */
-            error_message.nsCount = 0; /* Authority Record Count */
-            error_message.arCount = 0; /* Additional Record Count */
-
-            encode_msg(&error_message, &resp_ptr);
-            int len = resp_ptr - response_msg - 2;
-            resp_ptr = response_msg;
-            put16bits(&resp_ptr,len);
-
-            int bytes_written = write(client_socket,response_msg,sizeof(message_t) + 2);
-            if (bytes_written <= 0)
-            {
-                perror("error in write");
+                perror("Failed to read from socked");
+                exit(0);
             }
         }
-    }
-    else if (bytes_read  == 0)
-    {
-        // socket closed from the client.
     }
     else
     {
-        perror("socket read error \n");
+        exit(0);
     }
+
+    message_t dns_request_msg;
+    memset(&dns_request_msg, 0x00, sizeof(dns_request_msg));
+    decode_dns_msg( &dns_request_msg, buffer, dns_request_length);
+    int incoming_request_id = dns_request_msg.id;
+    print_message(&dns_request_msg);
+
+    // enter critical section
+    // 1. Open file
+    // 2. Write to the file.
+    // 3. Close the file
+    // Exit Critical section
+    sem_wait(&sem_io);
+    updatefile_requested(fp,dns_request_msg.questions->qName);
+    if (dns_request_msg.questions->qType != AAAA_Resource_RecordType)
+    {
+        updatefile_unimplemented_request(fp);
+    }
+    sem_post(&sem_io);
+
+    if (dns_request_msg.questions->qType == AAAA_Resource_RecordType)
+    {
+        // Check if the request can be served from local cache
+
+        // send the DNS request to next level server
+        send_dns_request(upstream_server_sockfd, next_hierarchy_dns_server_name, buffer, dns_request_length);
+
+        // Wait for the response
+        memset(buffer, 0x00, SOCKET_BUFF_SIZE);
+        socklen_t len;
+        struct sockaddr_in     servaddr;
+        int bytes_received = recvfrom(upstream_server_sockfd, buffer, SOCKET_BUFF_SIZE,MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
+        if (bytes_received <= 0)
+        {
+            perror("error in recvfrom");
+        }
+
+        memset(&dns_request_msg, 0x00, sizeof(dns_request_msg));
+
+        decode_dns_msg( &dns_request_msg, buffer, bytes_received);
+        sem_wait(&sem_io);
+        if (dns_request_msg.byte.u.rcode == Ok_ResponseType)
+        {
+            updatefile_ipaddress(fp, &dns_request_msg);
+        }
+        sem_post(&sem_io);
+
+        // Send the response to the server
+        uint8_t *response_msg = malloc(sizeof(char) * bytes_received + 2);
+        uint8_t *resp_ptr = response_msg;
+        memset(response_msg, 0x00, sizeof(char) * (bytes_received + 2));
+
+        // add length
+        put16bits(&response_msg,bytes_received);
+
+        // add oroginal request
+        put16bits(&response_msg,incoming_request_id);
+
+        // copy the response received from the server
+        memcpy(response_msg, &buffer[2], bytes_received-2);
+
+        int bytes_written = write(client_socket,resp_ptr,bytes_received+2);
+        if (bytes_written <= 0)
+        {
+            perror("error in write");
+        }
+
+        // Update the cache
+    }
+    else
+    {
+        uint8_t *response_msg = malloc(1024);
+        uint8_t *resp_ptr = response_msg + 2;
+
+        // send Error Record for the Non-AAAA request
+        message_t error_message;
+        memset(&error_message, 0x00, sizeof(error_message));
+
+        // set id
+        error_message.id = incoming_request_id;
+
+        // Set the type of message
+        error_message.byte.u.qr = 1;
+        error_message.byte.u.Opcode = 0;
+        error_message.byte.u.aa = 0;
+        error_message.byte.u.tc = 0;
+        error_message.byte.u.rd = 0;
+        error_message.byte.u.ra = 0;
+        error_message.byte.u.z = 0;
+        error_message.byte.u.rcode = NotImplemented_ResponseType;
+
+        // leave most values intact for response
+        error_message.qdCount = 0; /* Question Count */
+        error_message.anCount = 0; /* Answer Record Count */
+        error_message.nsCount = 0; /* Authority Record Count */
+        error_message.arCount = 0; /* Additional Record Count */
+
+        encode_msg(&error_message, &resp_ptr);
+        int len = resp_ptr - response_msg - 2;
+        resp_ptr = response_msg;
+        put16bits(&resp_ptr,len);
+
+        int bytes_written = write(client_socket,response_msg,sizeof(message_t) + 2);
+        if (bytes_written <= 0)
+        {
+            perror("error in write");
+        }
+    }
+
     return NULL;
 }
 
